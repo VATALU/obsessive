40cb337924db2d28bdf76f1fc8ddffbe7f0610b4
VATALU <2507978950@qq.com>
2018-05-14 14:47:00 +0800

    :pencil: 增加了一些常用类以及包扫描

---
+++ cgod/src/main/java/org/cgod/util/ConfigUtil.java
@@ -0,0 +1,60 @@
+package org.cgod.util;
+
+import org.cgod.ConfigConstant;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Enumeration;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Properties;
+
+
+/**
+ * 配置文件读取类
+ */
+public class ConfigUtil {
+    private static final Map<String,String> CONFIG_MAP;
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(ConfigUtil.class);
+
+    static {
+        Properties props;
+        InputStream is = null;
+        CONFIG_MAP = new HashMap<String, String>();
+        try {
+            is = Thread.currentThread().getContextClassLoader().getResourceAsStream(ConfigConstant.CONFIG_FILE);
+            if (is == null) {
+                throw new FileNotFoundException(ConfigConstant.CONFIG_FILE + " file is not found ");
+            }
+            props = new Properties();
+            props.load(is);
+            Enumeration en=props.propertyNames();
+            while (en.hasMoreElements()) {
+                String key=(String) en.nextElement();
+                String property=props.getProperty(key);
+                CONFIG_MAP.put(key, property);
+            }
+        } catch (IOException e) {
+            LOGGER.error("load properties file failure ", e);
+        } finally {
+            if (is != null) {
+                try {
+                    is.close();
+                } catch (IOException e) {
+                    LOGGER.error("close input stream failure ", e);
+                }
+            }
+        }
+    }
+
+    public static String getAppBasePackage() {
+        return CONFIG_MAP.get(ConfigConstant.APP_BASE_PACKAGE);
+    }
+
+
+}
+
